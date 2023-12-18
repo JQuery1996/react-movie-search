f06923bb52547655795d58bad51cc66e5d00be73
KenanAsaad <kinan.bassam.assad@gmail.com>
2023-12-18 16:26:49 +0400

    fix(progressbar): adding progress bar style to app.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { MotionLazyContainer } from "components/animate";
 import "./App.css";
 import { Router } from "routes";
+import { ProgressBarStyle } from "components/progressBar";
 
 function App() {
   return (
     <MotionLazyContainer>
+      <ProgressBarStyle />
       <Router />
     </MotionLazyContainer>
   );
